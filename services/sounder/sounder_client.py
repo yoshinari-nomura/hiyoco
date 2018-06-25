a9337903a4df37d03fa37b9a949a51525041b92d
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2018-06-25 17:24:10 +0900

    Fix typo to specify port

--- services/sounder/sounder_client.py
+++ services/sounder/sounder_client.py
@@ -10,7 +10,7 @@ except:
     port = 50051
 
 def run():
-    channel = grpc.insecure_channel('localhost' + str(port))
+    channel = grpc.insecure_channel('localhost:' + str(port))
     stub = hiyoco.sounder.service_pb2_grpc.SounderStub(channel)
     response = stub.SayEvent(hiyoco.calendar.event_pb2.Event(summary='Test event',description="This is test"))
     print(response)
