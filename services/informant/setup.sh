5cfffc82bcd69665cd453a12de2079d7585cc769
Ryota Nishi <nishi@swlab.cs.okayama-u.ac.jp>
2018-06-25 12:03:08 +0900

    Modify setup.sh to install python library and compile proto

--- services/informant/setup.sh
+++ services/informant/setup.sh
@@ -2,6 +2,23 @@
 
 SH_PATH=$(dirname $0)
 
+echo "Start setup for informant."
+echo "Python library will be installed. Are you OK? [Y/n]"
+read ANSWER
+
+case $ANSWER in
+    "" | "Y" | "y" | "yes" | "Yes" | "YES" ) pip install -r $SH_PATH/requirements/development.txt;;
+    * ) exit 0;;
+esac
+
+echo "Start compile proto file."
+$SH_PATH/compile_proto.sh
+echo "Done"
+
+echo "Set slack-token, post destination channel, and bot display name."
+echo "You can get slack-token from following URL."
+echo "https://api.slack.com/custom-integrations/legacy-tokens"
+
 echo -n YOUR_SLACK_TOKEN:
 read token
 echo -n CHANNEL_NAME:
