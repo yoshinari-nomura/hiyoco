9821d8aaec4dbae2358e14c37cc67cbffb64c07b
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2018-06-25 14:07:15 +0900

    Make it possible to specify port number when lounch server and test server

--- services/sounder/sounder_client.py
+++ services/sounder/sounder_client.py
@@ -4,8 +4,13 @@ import hiyoco.calendar.event_pb2_grpc
 import hiyoco.sounder.service_pb2
 import hiyoco.sounder.service_pb2_grpc
 
+try:
+    port = sys.argv[1]
+except:
+    port = 50051
+
 def run():
-    channel = grpc.insecure_channel('localhost:50051')
+    channel = grpc.insecure_channel('localhost' + str(port))
     stub = hiyoco.sounder.service_pb2_grpc.SounderStub(channel)
     response = stub.SayEvent(hiyoco.calendar.event_pb2.Event(summary='Test event',description="This is test"))
     print(response)
